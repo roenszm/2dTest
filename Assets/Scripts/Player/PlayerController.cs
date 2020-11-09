742e30823a243e4dabdfe1c3b1f0b14b61d6e95e
roenszm <roenszm@163.com>
2020-11-09 22:07:36 +0800

    add gameobj--player; horizontal move

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float speed = 2;
+    public float jumpForce = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");    // -1 ~ 1
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+        
+        if (horizontalInput < 0)
+        {
+            transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+        }
+        if (horizontalInput > 0)
+        {
+            transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+        }
+
+    }
+
+}
